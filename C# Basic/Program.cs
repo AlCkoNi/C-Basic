2366e8b4d48b45ce08780045efa4bdb897ccd10c
AlCkoNi <Tolibjon2004@yandex.ru>
2024-04-20 13:50:21 +0500

    Добавьте файлы проекта.

---
+++ C# Basic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+
+namespace C__Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public class C_sharp_Basic
+    {
+
+    }
+}

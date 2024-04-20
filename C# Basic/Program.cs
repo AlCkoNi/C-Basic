5d90edbf0ed0591c24404943f4f592aeb8fdc4dd
AlCkoNi <Tolibjon2004@yandex.ru>
2024-04-20 14:00:30 +0500

    leesson 1

--- C# Basic/Program.cs
+++ C# Basic/Program.cs
@@ -8,11 +8,18 @@ namespace C__Basic
     {
         static void Main(string[] args)
         {
-            
+            C_sharp_Basic c = new C_sharp_Basic();
+            c.lesson1();
         }
     }
     public class C_sharp_Basic
     {
-
+        public void lesson1() //lESSON - 1 wHAT iS .nET aND CLI, CLR, SDK
+        {
+            //.Net - is Free.Cross - platform.Open source.
+            // A developer platform for building all your apps.
+            WriteLine("Hello World");
+        }
+        
     }
 }
